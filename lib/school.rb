593efde1159e6037fa5848bfba71bbe849b80257
Oroko <aoroko09@gmail.com>
2020-10-15 09:25:52 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+  attr_accessor :name, :roster, :grade 
+  
+   
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(grade,name)
+   @roster[name] ||= []
+   @roster[name] << grade
+  end
+  def grade(grade)
+    @roster.collect do |k,v|
+      if k == grade
+        v
+        
+      end
+    end
+    .flatten.compact
+  end
+
+  def sort 
+   new_hash = {}
+   @roster.each do |k,v|
+    new_hash[k] = v.sort
+    
+   end
+    new_hash
+  end
+  
+end
